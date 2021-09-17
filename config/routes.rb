6a56c2768a5992768340a64d4999fd9baffcce53
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-09-17 18:55:41 +0900

    add user detail page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get '/users/:id', to: 'users#show', as: 'user'
   end
   root to: "jobs#index"
   resources :jobs, only: [:new, :create, :show] do
