3134d794b7d9c610ffc1f1ad5736db3e7fe93051
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-09-19 07:40:50 +0900

    add profiles#new

--- app/controllers/company_profiles_controller.rb
+++ app/controllers/company_profiles_controller.rb
@@ -1,2 +1,23 @@
 class CompanyProfilesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
+  def new
+    @company_profile = CompanyProfile.new
+  end
+
+  def create
+    @company_profile = CompanyProfile.new(company_profile_params)
+    if @company_profile.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def company_profile_params
+    params.require(:company_profile).permit(:speciality, :content, :self_introduction, :conmapy_url, :contact).merge(user_id: current_user.id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
+    @company_profile = CompanyProfile.find_by(user_id: params[:id])
   end
 end
