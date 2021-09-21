f80b54ab1f812927179a850478aaee9890dcc748
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-09-21 10:44:18 +0900

    fix view of  user #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     @company_profile = CompanyProfile.find_by(user_id: params[:id])
-    if user_signed_in?
-      @follow = Follow.find_by(follow_id: params[:id], user_id: current_user.id)
+    if @company_profile.nil?
+      @company_profile = CompanyProfile.new
     end
+    # if user_signed_in?
+    #   @follow = Follow.find_by(follow_id: params[:id], user_id: current_user.id)
+    # end
     @follows = Follow.where(user_id: params[:id])
     @followers = Follow.where(follow_id: params[:id])
   end
