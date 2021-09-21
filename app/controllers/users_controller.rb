bb0f871f9ebdde38c02e45a1dc2010f035298582
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-09-21 11:23:41 +0900

    add favorites index on user #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    @jobs = Job.where(user_id: params[:id])
     @company_profile = CompanyProfile.find_by(user_id: params[:id])
     if @company_profile.nil?
       @company_profile = CompanyProfile.new
     end
-    # if user_signed_in?
-    #   @follow = Follow.find_by(follow_id: params[:id], user_id: current_user.id)
-    # end
+    @favorites = Favorite.where(user_id: params[:id])
     @follows = Follow.where(user_id: params[:id])
     @followers = Follow.where(follow_id: params[:id])
   end
