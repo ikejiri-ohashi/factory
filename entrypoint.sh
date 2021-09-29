2a1d3e4420a95f891ea23dd0ac710b42f773cbae
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-09-29 11:55:51 +0900

    miniprofiler, その他設定追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /factory/tmp/pids/server.pid
 
-# bundle exec rails db:create
-bundle exec rails db:migrate
+# RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
+# bundle exec rails db:create RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
 # bundle exec rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
