a6a86fe1c38be1d95e9178dada1e061cb8346ddc
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-10-13 11:42:59 +0900

    rubocopをcircleCIに導入

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,10 +10,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
     root_path
     # "https://www.factory-app.com/"
   end
-
-  def ensure_normal_user
-    if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
-    end
-  end
+  
 end
