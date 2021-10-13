d24ca8ad77fbfe5ba5f3ff94a4638fea87e06efe
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-10-13 10:09:45 +0900

    Rubocopの警告部分の修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,9 +12,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def ensure_normal_user
-    if resource.email == 'guest@example.com'
+    return unless resource.email == 'guest@example.com'
       redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
       # redirect_to "https://www.factory-app.com/", alert: 'ゲストユーザーは削除できません。'
-    end
   end
 end
