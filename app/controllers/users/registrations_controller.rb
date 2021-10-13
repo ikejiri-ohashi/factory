0103d6625c1e043eb3cb3b2455bee16404fae69a
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-10-13 11:47:57 +0900

    fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: :destroy
+  
   def after_sign_up_path_for(_resource)
     root_path
     # "https://www.factory-app.com/"
