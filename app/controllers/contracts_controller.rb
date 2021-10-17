76167abc687a4b5bfd288287ec12b42ffcf1ba00
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-10-17 18:19:14 +0900

    Rubocop 適用

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,9 +1,9 @@
 class ContractsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
-
+  # rubocop:disable all
   def create
     ordering = current_user.contracts.create(contracter_id: 2, job_id: params[:job_id])
     redirect_back(fallback_location: root_path)
   end
-
+  # rubocop:enable all
 end
