ce55aea84feddd562c3b3ae099facb83af22f22f
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-10-20 16:11:21 +0900

    ユーザー詳細画面のお気に入り一覧のN+1問題を解決

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @jobs = Job.where(user_id: params[:id])
     @company_profile = CompanyProfile.find_by(user_id: params[:id])
-    @favorites = Favorite.where(user_id: params[:id])
+    @favorites = Favorite.where(user_id: params[:id]).includes(:user)
     @follows = Follow.where(user_id: params[:id])
     @followers = Follow.where(follow_id: params[:id])
     @check_follow = Follow.find_by(user_id: current_user.id, follow_id: params[:id]) if user_signed_in?
