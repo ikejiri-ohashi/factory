26ff852053b6f3e86680518694970de4c3dfe477
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-10-20 17:54:27 +0900

    Followのselect文を削減

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @jobs = Job.where(user_id: params[:id])
     @company_profile = CompanyProfile.find_by(user_id: params[:id])
-    @favorites = Favorite.where(user_id: params[:id]).includes(:user)
-    @follows = Follow.where(user_id: params[:id])
-    @followers = Follow.where(follow_id: params[:id])
+    @favorites = Favorite.where(user_id: params[:id]).includes(:job)
+    @follows = Follow.where(user_id: params[:id]).includes(:user, :follow)
+    @followers = Follow.where(follow_id: params[:id]).includes(:user, :follow)
     @check_follow = Follow.find_by(user_id: current_user.id, follow_id: params[:id]) if user_signed_in?
-    @contracts = Contract.where(user_id: params[:id])
-    @accepts = Contract.where(contracter_id: params[:id])
+    @contracts = Contract.where(user_id: params[:id]).includes(:job)
+    @accepts = Contract.where(contracter_id: params[:id]).includes(:job)
   end
 end
