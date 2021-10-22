f470a9f59220a9e5be7f592a81cf51b2c69b0828
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-10-22 09:10:59 +0900

    ユーザー詳細画面にリクエスト送信フォームを設置

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,14 @@
 class RequestsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  # rubocop:disable all
+  def create
+    if request_id == params[:id]
+      ordering = current_user.contracts.create(request_id: params[:id], job_id: params[:job_id])
+      redirect_back(fallback_location: root_path)
+    else
+      ordering = current_user.contracts.create(request_id: params[:request_id], job_id: params[:id])
+      redirect_back(fallback_location: root_path)
+    end
+  end
+  # rubocop:enable all
 end
