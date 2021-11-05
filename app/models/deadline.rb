b2f84f5d4a90d70dea8ed2e6e9b1e3237a401ddf
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-11-05 13:32:57 +0900

    デザイン微調整

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -1,8 +1,13 @@
 class Deadline < ActiveHash::Base
   self.data = [
     { id: 1, name: '選択してください' },
-    { id: 2, name: '10月' },
-    { id: 3, name: '11月' }
+    { id: 2, name: '~ 11/19(金)' },
+    { id: 3, name: '~ 11/26(金)' },
+    { id: 4, name: '~ 12/3(金)' },
+    { id: 5, name: '~ 12/10(金)' },
+    { id: 6, name: '~ 12/17(金)' },
+    { id: 7, name: '~ 12/24(金)' },
+    { id: 8, name: '~ 12/31(金)' }
   ]
   include ActiveHash::Associations
   belongs_to :jobs
