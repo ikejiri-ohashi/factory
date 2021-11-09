f2f0c795265c10b6174c95df36a9c072407b59fd
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-11-09 11:43:08 +0900

    フォロー機能修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -4,14 +4,12 @@ class FollowsController < ApplicationController
   def create
     @follow = current_user.follows.create(follow_id: params[:user_id])
     @follow.save
-    @followers = Follow.where(follow_id: params[:user_id]).includes(:user, :follow)
     @check_follow = Follow.find_by(user_id: current_user.id, follow_id: params[:id])
   end
 
   def destroy
     @follow = Follow.find_by(follow_id: params[:user_id], user_id: current_user.id)
     @follow.destroy
-    @followers = Follow.where(follow_id: params[:user_id]).includes(:user, :follow)
     @check_follow = Follow.find_by(user_id: current_user.id, follow_id: params[:id])
   end
 end
