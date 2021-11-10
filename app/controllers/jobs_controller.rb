e9fd570197e365d5c1d8c65043f49453eaf2d177
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-11-10 16:44:59 +0900

    user#showのリクエスト送信リストの挙動を修正

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -88,8 +88,8 @@ class JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
     if @job.save
-      redirect_to root_url
-      # redirect_to "https://www.factory-app.com/jobs/#{@job.id}"
+      # redirect_to root_url
+      redirect_to "https://www.factory-app.com/jobs/#{@job.id}"
     else
       render :new
     end
@@ -113,8 +113,8 @@ class JobsController < ApplicationController
 
   def destroy
     @job.destroy
-    redirect_to root_url
-    # redirect_to "https://www.factory-app.com/"
+    # redirect_to root_url
+    redirect_to "https://www.factory-app.com/"
   end
 
   private
