5a78f92ecc3e0cb0d801fe76b8df8ce00bfa210f
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-11-12 13:25:44 +0900

    turbolink無効化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,11 @@
 require('jquery')
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "stylesheets/application.scss"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
